eee3579ed69a7d0add0742a574054d9745ea23e3
sharonchen9593 <sharonchen9593@gmail.com>
2017-06-21 20:07:13 -0700

    Make dropdown list working

--- client/src/components/searchBar.js
+++ client/src/components/searchBar.js
@@ -1,4 +1,11 @@
 angular.module('app')
 .component('searchBar', {
-  templateUrl: './src/views/searchBar.html'
-})
+  templateUrl: './src/views/searchBar.html',
+  bindings: {
+    makes: '<',
+    onChange: '<'
+  },
+  controller: function() {
+    console.log(this);
+  }
+});
