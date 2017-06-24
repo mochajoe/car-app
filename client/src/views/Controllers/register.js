c8e058b336047feea384886b95c8812cb118ef63
Anthony Nguyen <anguyee@gmu.edu>
2017-06-24 00:03:50 -0700

    modified the controller for the register page to alert when there is a username already in the database and if there is not a user in the database, it alerts the user that everything is good and directs them to the account page

--- client/src/views/Controllers/register.js
+++ client/src/views/Controllers/register.js
@@ -1,15 +1,27 @@
-app.controller("registerCtrl", function($scope,$http) {
+app.controller("registerCtrl", function($scope,$http,$rootScope,$window,$location) {
   $scope.register = function(user)
   {
     console.log(user);
     //TODO verify that passwords are the same and then invocate the post
     if(user.password === user.password2){
     $http.post('/register', user).then(function successCallback(user) {
+      if (user.data === null) {
+        //render out that this is already in the system, I'm going to use a temporary window alert for this now. We need to make this better
+        $window.alert("USER ALREADY EXISTS DUDE!!!!!, PICK ANOTHER DAMN NAME")
+      } else {
       $rootScope.currentUser = user;
-      console.log(user);
+      //alert a successful login
+      $window.alert("SUCCESS, REDIRECTING TO ACCOUNT PAGE!!!!!")
+      $location.url("/account");
+      console.log(user)
+      }
+
+      // console.log(user);
     }, function errorCallBack(response) {
       console.log(response);
     })
-  };
+  } else {
+    $window.alert("PASSWORDS DOT NOT MATCH!!!!!!!");
     }
+  }
 });
