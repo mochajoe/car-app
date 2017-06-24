75dfb6bf8bebcc7de62350e0d6b73e0c20f1a1aa
Anthony Nguyen <anguyee@gmu.edu>
2017-06-23 23:39:46 -0700

    added more comments to the login controller

--- client/src/views/Controllers/login.js
+++ client/src/views/Controllers/login.js
@@ -1,14 +1,14 @@
 app.controller("loginCtrl", function($scope,$http,$window,$rootScope,$location) {
   $scope.login = function(user)
   {
-    console.log(user);
-    $http.post('/login', user).then( (res) => {
-      $rootScope.currentUser = user;
-      console.log(res);
-      $location.url("/account"); //account is the profile
+    // console.log(user);
+    $http.post('/login', user).then( (res) => { //if this is a valid user
+      $rootScope.currentUser = user; //This is the OVERALL rootscope!!!!
+      console.log($rootScope); //whatever this is supposed to show this isn't working
+      $location.url("/account"); //this changes the location to /account
     },  (data) => {
       $window.alert("Error that is not a valid Account, please try again");
-
+      //the data element is not showing.
     })
   };
 });
